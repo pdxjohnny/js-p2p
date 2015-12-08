b09c9bcd620d105f58f52c1db15eddfcf0317f82
John Andersen <john.s.andersen@intel.com>
2015-12-08 13:18:18 -0800

    Example nodejs chat
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,51 +1,97 @@
+var util = require('util');
 var p2p = require('wrtc-p2p');
+var readlineSync = require('readline-sync');
 
-var have = 0;
-var needed = 2;
-var test_caller = new p2p.Client();
-var test_receiver = new p2p.Client();
+var main = function () {
+  var action = '';
+  if (process.argv.length > 2) {
+    var action = process.argv[2];
+  }
 
-var checkNeeded = function () {
-  have++;
-  console.log(have, 'responses recived');
-  if (have >= needed) {
-    console.log('Got all responses, test complete');
-    process.exit(0);
+  switch (action) {
+    case 'join':
+      joinChat();
+      break;
+    case 'create':
+      createChat();
+      break;
+    default:
+      console.log('Usage: %s (create or join)', process.argv[1]);
+      process.exit();
   }
 };
 
-test_caller.onmessage = function (data) {
-  console.log('test_caller:    ', data);
-  checkNeeded();
-};
-test_receiver.onmessage = function (data) {
-  console.log('test_receiver:  ', data);
-  checkNeeded();
+var createClient = function () {
+  var client = new p2p.Client();
+
+  client.onmessage = function (data) {
+    console.log('them:  '+ data.message);
+  };
+
+  return client;
 };
 
-console.log('Creating offer');
-test_caller.offer(function (offer) {
-  console.log('Offer created');
-  // Offer is sent to the receiver
-  console.log(offer);
-  console.log('Creating answer');
-  // The receiver creates an answer for the offerer
-  test_receiver.handleOffer(offer, function (answer) {
-    console.log('Created answer');
+var stdinChat = function (client) {
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  process.stdin.on('data', function (text) {
+    // Remove the newline
+    text = text.slice(0, -1);
+    if (text === 'exit') {
+      process.exit();
+    }
+    client.send({
+      'message': text
+    });
+  });
+};
+
+var joinChat = function () {
+  var client = createClient();
+
+  // The receiver get an offer for the offerer
+  // Here the receiver is inputing the offer
+  var offer = readlineSync.question('Input offer: ', {
+    hideEchoBack: false // The typed text on screen is hidden by `*` (default).
+  });
+  console.log('');
+  console.log('Creating answer...');
+  client.handleOffer(offer, function (answer) {
+    console.log('Created answer, send this back to the offerer');
     console.log(answer);
-    // The offerer gets the receivers answer
-    test_caller.answerRecieved(answer);
-    test_caller.onconnect = function () {
-      console.log('Caller connected');
-      test_caller.send({
-        'message': 'hello from caller'
-      });
-    };
-    test_receiver.onconnect = function () {
-      console.log('Receiver connected');
-      test_receiver.send({
-        'message': 'hello from receiver'
-      });
+    console.log('');
+    console.log('Connecting...');
+    client.onconnect = function () {
+      console.log('Connected');
+      console.log('');
+      stdinChat(client);
     };
   });
-});
+};
+
+var createChat = function () {
+  var client = createClient();
+
+  console.log('Creating offer...');
+  client.offer(function (offer) {
+    console.log('Offer created, send it to who you want to chat with');
+    // Offer is sent to the receiver
+    console.log(offer);
+    console.log('');
+    // The receiver creates an answer for the offerer
+    // Here the offerer is inputing the awnser
+    var answer = readlineSync.question('Input answer: ');
+    console.log('');
+    // The offerer gets the receivers answer
+    console.log('Connecting...');
+    client.answerRecieved(answer);
+    client.onconnect = function () {
+      console.log('Connected');
+      console.log('');
+      stdinChat(client);
+    };
+  });
+};
+
+main();
